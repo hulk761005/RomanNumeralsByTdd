3c91de2bc8ba6a555f4cd95900d787566d7a2b61
hulkchou <hulk.chou@ailabpte.co>
2024-07-25 17:47:12 +0800

    refactor: by chatgpt

--- RomanNumeralsByTdd/RomanNumeralsConverter.cs
+++ RomanNumeralsByTdd/RomanNumeralsConverter.cs
@@ -4,65 +4,24 @@ namespace RomanNumeralsByTdd;
 
 public class RomanNumeralsConverter
 {
+    private static readonly (int Value, string Numeral)[] RomanNumerals = 
+    {
+        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+        (1, "I")
+    };
+
     public string Convert(int number)
     {
         var sb = new StringBuilder();
-        while (number > 0)
+        
+        foreach (var (value, numeral) in RomanNumerals)
         {
-            switch (number)
+            while (number >= value)
             {
-                case >= 1000:
-                    sb.Append('M');
-                    number -= 1000;
-                    break;
-                case >= 900:
-                    sb.Append("CM");
-                    number -= 900;
-                    break;
-                case >= 500:
-                    sb.Append('D');
-                    number -= 500;
-                    break;
-                case >= 400:
-                    sb.Append("CD");
-                    number -= 400;
-                    break;
-                case >= 100:
-                    sb.Append('C');
-                    number -= 100;
-                    break;
-                case >= 90:
-                    sb.Append("XC");
-                    number -= 90;
-                    break;
-                case >= 50:
-                    sb.Append('L');
-                    number -= 50;
-                    break;
-                case >= 40:
-                    sb.Append("XL");
-                    number -= 40;
-                    break;
-                case >= 10:
-                    sb.Append('X');
-                    number -= 10;
-                    break;
-                case >= 9:
-                    sb.Append("IX");
-                    number -= 9;
-                    break;
-                case >= 5:
-                    sb.Append('V');
-                    number -= 5;
-                    break;
-                case >= 4:
-                    sb.Append("IV");
-                    number -= 4;
-                    break;
-                default:
-                    sb.Append('I');
-                    number -= 1;
-                    break;
+                sb.Append(numeral);
+                number -= value;
             }
         }
 
